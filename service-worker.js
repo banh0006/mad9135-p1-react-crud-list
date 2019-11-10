acd223e45fd30d0e71d73b4bc7812b9f8d4ab0c7
banh0006 <banh0006@algonquinlive.com>
2019-11-09 23:34:25 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/banh0006/mad9135-p1-react-crud-list/precache-manifest.abf03af242bd214468306fea55e88b75.js"
+  "/mad9135-p1-react-crud-list/precache-manifest.8e2dec8f7bea451fdb9eff2847e01dd2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/banh0006/mad9135-p1-react-crud-list/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mad9135-p1-react-crud-list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
