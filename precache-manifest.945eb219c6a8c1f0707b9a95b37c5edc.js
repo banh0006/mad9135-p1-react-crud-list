008dbe26302e0c708e981f038050870d295cd269
banh0006 <banh0006@algonquinlive.com>
2020-08-23 11:15:32 -0400

    Updates

---
+++ precache-manifest.945eb219c6a8c1f0707b9a95b37c5edc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb027e4b41be0e89d2481b78e2f98d23",
+    "url": "/mad9135-p1-react-crud-list/index.html"
+  },
+  {
+    "revision": "bbfc9260531c34a172a7",
+    "url": "/mad9135-p1-react-crud-list/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "b22a70a0e5c57abde2c7",
+    "url": "/mad9135-p1-react-crud-list/static/css/main.ff7f972e.chunk.css"
+  },
+  {
+    "revision": "bbfc9260531c34a172a7",
+    "url": "/mad9135-p1-react-crud-list/static/js/2.7497e9ef.chunk.js"
+  },
+  {
+    "revision": "b22a70a0e5c57abde2c7",
+    "url": "/mad9135-p1-react-crud-list/static/js/main.eee648d1.chunk.js"
+  },
+  {
+    "revision": "93c9bd13cc9b6552c3ad",
+    "url": "/mad9135-p1-react-crud-list/static/js/runtime-main.c813c03e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/mad9135-p1-react-crud-list/static/media/logo.25bf045c.svg"
+  }
+]);
